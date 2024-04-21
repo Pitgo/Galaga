6564ed824413567748127851a159e5388fd4b07f
RaSsaS-GD <jdam137@gmail.com>
2024-04-21 16:43:31 -0500

    GO

--- Code/Galaga/font.py
+++ Code/Galaga/font.py
@@ -2,4 +2,4 @@ import pygame
 
 
 def get_font(size):  # Returns Press-Start-2P in the desired size
-    return pygame.font.Font("Font/font.ttf", size)
+    return pygame.font.Font("Font/fonts.ttf", size)
